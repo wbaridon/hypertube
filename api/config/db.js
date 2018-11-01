8fdc3e09a94c02a350f6e39637452c8d8bda78df
Wenceslas BARIDON <wbaridon@e3r12p15.42.fr>
2018-11-01 16:18:55 +0100

    Fichier connexion a la DB

---
+++ api/config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/hypertube', { useNewUrlParser: true });
+
+mongoose.connection.once('open', function (){
+  console.log('Connexion DB: OK')
+}).on('error', function(error){
+  console.log('Connection error:', error);
+});
