ee42e70ec077886b1ed752b9279284f0c49cb083
Guillaume RICHARD <guiricha@e3r3p15.42.fr>
2018-11-02 17:51:04 +0100

    small styles example

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { Grid, Button } from '@material-ui/core';
+import { FormattedMessage } from 'react-intl';
+import { withStyles } from '@material-ui/core/styles';
+
+const myStyles = {
+  root: {
+    width: '52px',
+  },
+};
 
 class Home extends React.Component {
   constructor() {
     super();
     this.state = {
-      fakestate: 283,
+      fakestate: '283',
     };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(fakestate) {
+    this.setState({ fakestate });
   }
 
   render() {
     const { fakestate } = this.state;
+    const { classes } = this.props;
+
     return (
       <Grid container spacing={0} alignItems="center" justify="center">
-        <Grid item xs={11} sm={10} md={9} key={fakestate}>
-          <div>{fakestate}</div>
+        <Grid item xs={11} sm={10} md={9}>
+          <FormattedMessage id="example" className={classes.root} />
+          <Button onClick={this.handleClick} label={fakestate}>{fakestate}</Button>
         </Grid>
       </Grid>
     );
   }
 }
 
+Home.propTypes = {
+  classes: PropTypes.shape({}).isRequired,
+};
+
 Home.url = '/';
-export default Home;
+export default withStyles(myStyles)(Home);
