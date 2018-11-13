eb942840f57b94e5ddaf575afa3ecd65c97a58d1
Wenceslas BARIDON <wbaridon@e2r12p7.42.fr>
2018-11-13 10:46:16 +0100

    Debut de la gestion du login

--- api/routes/oAuthRouter.js
+++ api/routes/oAuthRouter.js
@@ -15,26 +15,41 @@ const credentials = {
     tokenPath: '/oauth/token'
    }
 };
-// Je propose que l'on laisse Google API de cote pour le moment.
-// twitter, github, google, 42, et inscription normal
-// Link pour github https://github.com/login/oauth/authorize?scope=user&client_id=ca9256d31baf98f55288
+
 oAuthRouter
   .post('/register', (req, res) => {
-    let provider = req.body.provider
+    getToken(req.body.provider, req.url, req.body.clientCode, credentials)
+    .then(token => {
+      getUserFrom(req.body.provider, token).then(user => res.send(user))
+    }).catch(error => res.send(error))
+  })
+  .post('/login', (req, res) => {
+    getToken(req.body.provider, req.url, req.body.clientCode, credentials)
+    .then(token => {
+      getUserFrom(req.body.provider, token).then(user =>
+        // Au register il faudra checker que le compte est pas deja pris
+        // Il faut renvoyer un token jwt par rapport au compte ou le token oauth ?
+         res.send('non fonctionnel')
+       );
+    }).catch(error => res.send(error))
+  })
+
+function getToken(provider, path, clientCode, credentials) {
+  return new Promise((resolve, reject) => {
     getCredentials(provider, credentials).then(credentials => {
       const oauth2 = require('simple-oauth2').create(credentials);
       const tokenConfig = {
-        code: req.body.clientCode,
-        redirect_uri: 'http://localhost:8080/register'
+        code: clientCode,
+        redirect_uri: `http://localhost:8080${path}/${provider}`
       };
       oauth2.authorizationCode.getToken(tokenConfig).then(result => {
-        const accessToken = oauth2.accessToken.create(result);
-        const token = accessToken.token.access_token
-        getUserFrom(provider, token).then(user => res.send(user))
-      }).catch (error => { res.send({'error': 'Access Token Error'}); })
-    })
-  })
-
+         const accessToken = oauth2.accessToken.create(result);
+         const token = accessToken.token.access_token
+         resolve(token);
+       }).catch (error => { reject({'error': 'Access Token Error'}); })
+    });
+  });
+}
 function getUserFrom(provider, token) {
   return new Promise ((resolve, reject) => {
     if (provider === 'github') {
@@ -102,39 +117,5 @@ function gitHubCredentials(credentials) {
      resolve(credentials)
     })
    }
-  /* Implemantation de la strategie google lire https://developers.google.com/identity/protocols/OpenIDConnect
-https://accounts.google.com/o/oauth2/v2/auth
-   const credentialsGoogle = {
-     client: {
-       id: '796040540786-mcs3ojd9c07558mkatt5m1j185pol941.apps.googleusercontent.com',
-       secret: 'S5_yCT_BnFVr5v9qM9Av9g6m'
-      },
-      auth: {
-       tokenHost: 'https://api.intra.42.fr',
-       tokenPath: '/oauth/token'
-      }
-   };
-   .post('/register/google', async (req, res) => {
-     const oauth2 = require('simple-oauth2').create(credentials);
-     const tokenConfig = {
-       code: req.body.clientCode,
-       redirect_uri: 'http://localhost:8080'
-     };
-     try {
-       const result =  await oauth2.authorizationCode.getToken(tokenConfig);
-       const accessToken = oauth2.accessToken.create(result);
-       const token = accessToken.token.access_token
-       axios.get('https://api.intra.42.fr/v2/me', { headers: {"Authorization": `Bearer ${token}`}}).then(response => {
-        var user = {
-          email: response.data.email,
-          login: response.data.login,
-          picture: response.data.image_url,
-          name: response.data.last_name,
-          firstname: response.data.first_name,
-          password: '',
-        }
-        res.send(user)
-      })
-    } catch (error) { console.log('Access Token Error', error.message); }
-  }) */
+
 module.exports = oAuthRouter;
