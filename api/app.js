f2d1d94d8f8251555f57c307ca2adbe0796abd03
Fabien RAHAINGOMANANA <fabrahaingo@hotmail.fr>
2018-11-14 19:31:07 +0100

    Update video stream (not working yet)

--- api/app.js
+++ api/app.js
@@ -24,42 +24,6 @@ app.get('/', function (req, res) {
   res.send('Hello world!');
 });
 
-app.get('/video', function(req, res) {
-  const videoTag = document.getElementById("videoPlayer");
-
-  console.log("coucou");
-
-  // creating the MediaSource
-  const myMediaSource = new MediaSource();
-  const url = URL.createObjectURL(myMediaSource);
-
-  // attaching the MediaSource to the video tag
-  videoTag.src = url;
-
-  // 1. Creating source buffers
-  const audioSourceBuffer = myMediaSource
-    .addSourceBuffer('audio/mp4; codecs="mp4a.40.2"');
-  const videoSourceBuffer = myMediaSource
-    .addSourceBuffer('video/mp4; codecs="avc1.64001e"');
-
-  // 2. download and add our audio/video to the SourceBuffers:
-  // audio SourceBuffer
-  fetch("http://localhost:3000/videos/music.mp4").then(function(response) {
-  // The data has to be a JavaScript ArrayBuffer
-  return response.arrayBuffer();
-  }).then(function(audioData) {
-    audioSourceBuffer.appendBuffer(audioData);
-  });
-
-  // video SourceBuffer
-  fetch("http://localhost:3000/videos/music.mp4").then(function(response) {
-  // The data has to be a JavaScript ArrayBuffer
-  return response.arrayBuffer();
-  }).then(function(videoData) {
-    videoSourceBuffer.appendBuffer(videoData);
-  });
-});
-
 app.listen(port, function () {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
--- client/src/components/video/video.jsx
+++ client/src/components/video/video.jsx
@@ -11,26 +11,55 @@ class Video extends React.Component {
   constructor() {
     super();
     this.state = {
+
     };
   }
 
   componentDidMount() {
     Axios.get('http://localhost:3000/videos/music.mp4', {
 
-    }).then((result) => {
-      console.log(result);
+    }).then(() => {
+      // Define video & audio formats
+      const audioFormat = 'audio/mp4; codecs=mp4a.40.2';
+      const videoFormat = 'video/mp4; codecs=avc1.64001e';
+
+      function sourceOpen() {
+        const mediaSource = this;
+        const audioSourceBuffer = mediaSource.addSourceBuffer(audioFormat);
+        const videoSourceBuffer = mediaSource.addSourceBuffer(videoFormat);
+        fetch('http://localhost:3000/videos/music.mp4').then((response) => {
+        // The data has to be a JavaScript ArrayBuffer
+          return response.arrayBuffer();
+        }).then((audioData) => {
+          audioSourceBuffer.appendBuffer(audioData);
+        });
+        fetch('http://localhost:3000/videos/music.mp4').then((response) => {
+        // The data has to be a JavaScript ArrayBuffer
+          return response.arrayBuffer();
+        }).then((videoData) => {
+          videoSourceBuffer.appendBuffer(videoData);
+        });
+      }
+
+      if ('MediaSource' in window && MediaSource.isTypeSupported(audioFormat) && MediaSource.isTypeSupported(videoFormat)) {
+        const myMediaSource = new MediaSource();
+        const url = URL.createObjectURL(myMediaSource);
+        const videoTag = document.getElementById('videoPlayer');
+        videoTag.src = url;
+        console.log(myMediaSource.readyState);
+        myMediaSource.addEventListener('sourceopen', sourceOpen);
+      } else {
+        console.error('woops...');
+      }
     });
-    console.log(document.getElementById('videoPlayer'));
   }
 
   render() {
-    const { patate } = this.state;
-
     return (
       <React.Fragment>
         <video id="videoPlayer" src="http://localhost:3000/videos/music.mp4" width="1280px" height="720px" controls>
+          <track kind="captions" />
         </video>
-        <div>{`ma patateibale asdas ${patate}!`}</div>
       </React.Fragment>
     );
   }
