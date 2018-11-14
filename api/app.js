30a2258cb1526a6153807a61dca2e3cd7b6a4aee
Fabien RAHAINGOMANANA <fabrahaingo@hotmail.fr>
2018-11-14 14:42:04 +0100

    test

--- api/app.js
+++ api/app.js
@@ -27,6 +27,8 @@ app.get('/', function (req, res) {
 app.get('/video', function(req, res) {
   const videoTag = document.getElementById("videoPlayer");
 
+  console.log("coucou");
+
   // creating the MediaSource
   const myMediaSource = new MediaSource();
   const url = URL.createObjectURL(myMediaSource);
@@ -39,6 +41,23 @@ app.get('/video', function(req, res) {
     .addSourceBuffer('audio/mp4; codecs="mp4a.40.2"');
   const videoSourceBuffer = myMediaSource
     .addSourceBuffer('video/mp4; codecs="avc1.64001e"');
+
+  // 2. download and add our audio/video to the SourceBuffers:
+  // audio SourceBuffer
+  fetch("http://localhost:3000/videos/music.mp4").then(function(response) {
+  // The data has to be a JavaScript ArrayBuffer
+  return response.arrayBuffer();
+  }).then(function(audioData) {
+    audioSourceBuffer.appendBuffer(audioData);
+  });
+
+  // video SourceBuffer
+  fetch("http://localhost:3000/videos/music.mp4").then(function(response) {
+  // The data has to be a JavaScript ArrayBuffer
+  return response.arrayBuffer();
+  }).then(function(videoData) {
+    videoSourceBuffer.appendBuffer(videoData);
+  });
 });
 
 app.listen(port, function () {
