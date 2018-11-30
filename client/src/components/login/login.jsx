924d1ab694b2081e03bb614f8bf90441c547e8e4
lord0gnome <lord0gnome@gmail.com>
2018-11-30 13:37:11 +0100

    added cookie creation upon login

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -5,8 +5,19 @@ import { withSnackbar } from 'notistack';
 import { Typography } from '@material-ui/core';
 import { loginUser } from 'Actions/index';
 import { withTheme } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
 import LoginCard from './login-card';
 import './autocomplete-fix.css';
+
+function createCookie(name, value, days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    var expires = "; expires=" + date.toGMTString();
+  }
+  else var expires = "";
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
 
 const mapDispatchToProps = (dispatch) => {
   return ({
@@ -16,7 +27,9 @@ const mapDispatchToProps = (dispatch) => {
 
 const mapStateToProps = (state) => {
   return ({
-    loginState: state.user.loginState,
+    loggedIn: state.user.loggedIn,
+    user: state.user,
+    lastAction: state.user.lastAction,
   });
 };
 
@@ -28,21 +41,23 @@ class Login extends React.Component {
         userName: '',
         password: '',
       },
-      loginState: props.loginState,
+      user: props.user,
     };
     this.handleLogin = this.handleLogin.bind(this);
     this.handleStateChange = this.handleStateChange.bind(this);
   }
 
   static getDerivedStateFromProps(props, state) {
-    const { loginState, enqueueSnackbar } = props;
-    if (loginState !== state.loginState) {
-      if (loginState === 'LOGIN_USER_ERROR') {
+    const { user, enqueueSnackbar, lastAction } = props;
+
+    if (lastAction !== state.lastAction) {
+      if (lastAction === 'LOGIN_USER_ERROR') {
         enqueueSnackbar('Login Error!', { variant: 'error' });
-      } else if (loginState === 'LOGIN_USER_SUCCESS') {
+      } else if (lastAction === 'LOGIN_USER_SUCCESS') {
         enqueueSnackbar('Login Success!', { variant: 'success' });
+        createCookie('userToken', user.token, 7);
       }
-      return ({ loginState });
+      return ({ user });
     }
     return null;
   }
@@ -64,14 +79,21 @@ class Login extends React.Component {
   }
 
   render() {
+    const { loggedIn, lastAction } = this.props;
     const { currentUser } = this.state;
-    const { loginState } = this.state;
+    const { user } = this.state;
 
-    if (loginState === 'LOGIN_USER_IN_PROGRESS') {
+    if (lastAction === 'LOGIN_USER_IN_PROGRESS') {
       return (<Typography>LOADING</Typography>);
     }
-    if (loginState === 'LOGIN_USER_SUCCESS') {
-      return (<Typography>{`Hello ${currentUser.userName}!`}</Typography>);
+    if (loggedIn === 'true') {
+      return (
+        <React.Fragment>
+          <Typography>
+            {`Hello ${user.data.firstName} ${user.data.lastName}!`}
+          </Typography>
+          <Avatar src={`http://localhost:3000/images/${user.data.picture}`} />
+        </React.Fragment>);
     }
     return (<LoginCard currentUser={currentUser} parentLoginHandle={this.handleLogin} parentStateChange={this.handleStateChange} />);
   }
@@ -79,7 +101,7 @@ class Login extends React.Component {
 
 Login.propTypes = {
   logIn: PropTypes.func.isRequired,
-  loginState: PropTypes.string.isRequired,
+  user: PropTypes.shape({}).isRequired,
   enqueueSnackbar: PropTypes.func.isRequired, // eslint-disable-line
   theme: PropTypes.shape({
     palette: PropTypes.shape({
@@ -89,6 +111,8 @@ Login.propTypes = {
       getContrastText: PropTypes.func.isRequired,
     }).isRequired,
   }).isRequired,
+  loggedIn: PropTypes.string.isRequired,
+  lastAction: PropTypes.string.isRequired,
 };
 
 export default withTheme()(withSnackbar(connect(mapStateToProps, mapDispatchToProps)(Login)));
