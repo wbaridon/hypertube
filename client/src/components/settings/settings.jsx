05de1ca21577ffd6e70b392a3f03058d3805d173
lord0gnome <lord0gnome@gmail.com>
2018-11-30 13:34:20 +0100

    added settings

--- client/src/components/settings/settings.jsx
+++ client/src/components/settings/settings.jsx
@@ -1,14 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getUserInfoPrivate } from 'Actions';
 
 class Settings extends Component {
+  componentDidMount = () => {
+    const { getUser, token } = this.props;
+    getUser(token);
+  }
+
   render() {
     const { user } = this.props;
+    console.log(user);
     return (
       <div>
         {user.email}
       </div>
-    )
+    );
   }
 }
 
@@ -16,10 +24,17 @@ class Settings extends Component {
 Settings.url = '/settings';
 Settings.propTypes = {
   user: PropTypes.shape({}).isRequired,
+  getUser: PropTypes.func.isRequired,
+  token: PropTypes.string.isRequired,
 };
 
-const mapStateToProps = (state) => ({
-  user: state.user
+const mapStateToProps = state => ({
+  token: state.user.token,
+  user: state.user,
 });
 
-export default connect(mapStateToProps)(Settings);
+const mapDispatchToProps = dispatch => ({
+  getUser: token => dispatch(getUserInfoPrivate(token)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Settings);
