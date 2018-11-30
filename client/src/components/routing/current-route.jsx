20e4174c9aa114d20fe83fecbd128d32d1607e56
lord0gnome <lord0gnome@gmail.com>
2018-11-30 13:39:03 +0100

    added route for settings

--- client/src/components/routing/current-route.jsx
+++ client/src/components/routing/current-route.jsx
@@ -4,11 +4,13 @@ import Home from '../home/home';
 import Register from '../register/register';
 import Video from '../video/video';
 import Movie from '../movie/movie';
+import Settings from '../settings/settings';
 
 function CurrentRoute() {
   return (
     <Switch>
-      <Route exact path={Video.url} component={Video} />
+      <Route path={Video.url} component={Video} />
+      <Route path={Settings.url} component={Settings} />
       <Route path={Movie.url} component={Movie} />
       <Route path={Register.url} component={Register} />
       <Route exact path={Home.url} component={Home} />
