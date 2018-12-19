e486de1cbaf784ada23fb1d9f7f1fbff7dee637b
Guillaume RICHARD <guiricha@e2r8p15.42.fr>
2018-12-19 16:51:12 +0100

    removed useless import

--- client/src/components/video/video.jsx
+++ client/src/components/video/video.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { withRouter } from 'react-router-dom';
-import Axios from 'axios';
 import * as qs from 'query-string';
 
 
