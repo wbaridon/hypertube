5307d748f8fceae23c745cc53da4cdef4930eef6
Guillaume RICHARD <guiricha@e2r8p15.42.fr>
2018-12-19 15:45:40 +0100

    took better care of google Oauth, still need to access data by calling backend

--- client/src/components/register/register-card.jsx
+++ client/src/components/register/register-card.jsx
@@ -93,7 +93,7 @@ class RegisterCard extends React.Component {
   }
 
   componentDidUpdate = async () => {
-    const { registerData, clearRegisterDataHandler } = this.props;
+    const { registerData, clearRegisterDataHandler, provider } = this.props;
     if (registerData.exists) {
       try {
         const img = await Axios({
@@ -103,10 +103,14 @@ class RegisterCard extends React.Component {
           timeout: TIMEOUT_API,
         });
         this.handleImageAdd(img.data);
+        let { firstName, lastName } = registerData;
+        if (provider === 'github') {
+          [firstName, lastName] = lastName.split(' ');
+        }
         this.setState({
           userName: registerData.userName,
-          firstName: registerData.firstName,
-          lastName: registerData.lastName,
+          firstName,
+          lastName,
           email: registerData.email,
           password: '',
         });
