73de7faa6f89cbb806dd961e7954ed2f002c59c5
Guillaume Richard <lord0gnome@gmail.com>
2018-12-19 02:06:38 +0100

    oups j\'ai oublie video! :D

--- client/src/components/video/video.jsx
+++ client/src/components/video/video.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import { withRouter } from 'react-router-dom';
+import Axios from 'axios';
+import * as qs from 'query-string';
+
 
 const styles = {
 
 };
 
 class Video extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-
+      videoHash: qs.parse(props.location.search, { ignoreQueryPrefix: true }).videoHash,
     };
   }
 
   render() {
+    const { videoHash } = this.state;
     return (
       <React.Fragment>
         <video id="videoPlayer" controls muted>
-          <source src="http://localhost:3000/video" />
+          <source src={Axios({
+            method: 'get',
+            url: 'http://localhost:3000/video',
+            params: {
+              videoHash,
+            },
+            timeout: TIMEOUT_API,
+          })}
+          />
           <track kind="captions" default />
         </video>
       </React.Fragment>
@@ -28,7 +41,10 @@ class Video extends React.Component {
 
 Video.propTypes = {
   classes: PropTypes.shape({}).isRequired,
+  location: PropTypes.shape({
+    search: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 Video.url = '/video';
-export default withStyles(styles)(Video);
+export default withRouter(withStyles(styles)(Video));
