452c46e7b4920b8291c1553801bb4e90a244a8dc
Guillaume RICHARD <guiricha@e3r2p18.42.fr>
2019-01-10 14:47:14 +0100

    added token to form for imageadd

--- client/src/api_tools/update-user-image.js
+++ client/src/api_tools/update-user-image.js
@@ -1,6 +1,7 @@
 import Axios from 'axios';
 
-export default function updateUserImageAPI(formData) {
+export default function updateUserImageAPI(token, formData) {
+  formData.append('token', token);
   return Axios({
     method: 'post',
     url: 'http://localhost:3000/user/updatePicture',
