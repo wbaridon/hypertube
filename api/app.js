3c7a936d5dae04da6aa7dff397ad71c806e33cd9
Fabien RAHAINGOMANANA <fabrahaingo@hotmail.fr>
2019-01-17 19:49:27 +0100

    cleaned code + some small fixes

--- api/app.js
+++ api/app.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const torrentStream = require('torrent-stream');
-const magnetLink = require('magnet-link')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const db = require('./config/db');
 const fs = require('fs');
-const ffmpeg = require('fluent-ffmpeg');
 var schedule = require('node-schedule');
 const app = express();
 const hostname = 'localhost';
@@ -21,80 +18,16 @@ const userRouter = require('./routes/userRouter');
 const oAuthRouter = require('./routes/oAuthRouter');
 const libraryRouter = require('./routes/libraryRouter');
 const movieRouter = require('./routes/movieRouter');
+const torrentRouter = require('./routes/torrentRouter');
 
 app.use(express.static('assets'))
 app.use('/user', userRouter);
 app.use('/library', libraryRouter);
 app.use('/oAuth', oAuthRouter);
+app.use('/video', torrentRouter);
 
 app.get('/', function (req, res) {
   res.send('Hello world!');
-});
-
-app.get('/video', function(req, res) {
-
-  // Get torrent hash
-  // const torrent_hash = '3D1E3C092836AF4F3C21C38C09E1F5550E137A32'; // New Work Minute
-  const torrent_hash = req.query.videoHash;
-
-  // Make usable magnet link
-  const torrent_magnet = 'magnet:?xt=urn:btih:' + torrent_hash;
-
-
-  // Using torrent_stream
-  var engine = torrentStream(torrent_magnet);
-
-  engine.on('ready', function() {
-    // Iterates for each file linked in torrent
-    engine.files.forEach(function(file) {
-      let fileFormat = file.name.substr(file.name.length - 4);
-      const fileSize = file.length
-
-      // Check if server has something left to return
-      const range = req.headers.range
-
-      // If yes:
-      if (range && (fileFormat === '.mp4')) {
-        const parts = range.replace(/bytes=/, "").split("-")
-        console.log('filename:', file.name)
-        console.log(fileFormat);
-        console.log(fileSize)
-        console.log(parts)
-        const startByte = parseInt(parts[0], 10)
-        const endByte = parts[1]
-        ? parseInt(parts[1], 10)
-        : fileSize-1
-        const chunksize = (endByte-startByte)+1
-
-        // Create video stream from startByte to endByte
-        var stream = file.createReadStream({
-          start: startByte,
-          end: endByte
-        })
-
-        if (fileFormat === '.mp4') { // Is an mp4 file
-          const head = {
-            'Content-Range': `bytes ${startByte}-${endByte}/${fileSize}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': 'video/mp4',
-          }
-          res.writeHead(206, head)
-        }
-        // Pipe what was returned so far
-        stream.pipe(res)
-      } else if (fileFormat === '.mp4') {
-        const head = {
-          'Content-Length': fileSize,
-          'Content-Type': 'video/mp4',
-        }
-        res.writeHead(200, head)
-      fs.createReadStream(file.path).pipe(res)
-    }
-  });
-})
-
-
 });
 
 app.listen(port, function () {
---
+++ api/routes/torrentRouter.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const torrentRouter = express.Router();
+const torrentStream = require('torrent-stream');
+const ffmpeg = require('fluent-ffmpeg');
+const mimeTypes = require('../utils/mimeTypes.js');
+
+function getExtention(fileName) {
+  return fileName.substring(fileName.lastIndexOf('.'));
+}
+
+function getMagnet(hash) {
+  const magnet = 'magnet:?xt=urn:btih:';
+  return magnet.concat(hash);
+}
+
+function isVideo(mime) {
+  return mimeTypes[mime] !== undefined ? true : false;
+}
+
+torrentRouter
+  .get('/', function(req, res) {
+  let file;
+  // Create magnet from hash
+  const torrent_magnet = getMagnet(req.query.videoHash);
+  // Using torrent_stream
+  var engine = torrentStream(torrent_magnet);
+  engine.on('ready', function() {
+    // Iterates for each file linked in torrent
+    engine.files.forEach((current) => {
+      let mime = getExtention(current.name);
+      if (!isVideo(mime)) {
+        return;
+      } else if (file && current.length < file.length) {
+        return;
+      } else {
+        file = current;
+      }
+    });
+    if (!file) {
+      engine.removeAllListeners();
+      engine.destroy();
+      reject(new Error('No video found on this torrent'));
+    }
+
+    file.select();
+
+    const fileSize = file.length
+    // Check if server has something left to return
+    const range = req.headers.range
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-")
+      console.log('filename:', file.name)
+      console.log(fileSize)
+      console.log(parts)
+      const startByte = parseInt(parts[0], 10)
+      const endByte = parts[1]
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+      const chunksize = (endByte-startByte)+1
+      // Create video stream from startByte to endByte
+      var stream = file.createReadStream({
+        start: startByte,
+        end: endByte
+      })
+      const head = {
+        'Content-Range': `bytes ${startByte}-${endByte}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(206, head)
+      // Pipe what was returned so far
+      stream.pipe(res)
+    }
+    else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(200, head)
+      fs.createReadStream(file.path).pipe(res)
+    }
+  });
+})
+
+module.exports = torrentRouter;
