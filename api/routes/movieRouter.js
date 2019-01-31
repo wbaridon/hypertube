06b64c07708e3d05a4dab1a7b8beded1e938e1c2
Wenceslas BARIDON <wbaridon@e3r2p18.42.fr>
2019-01-31 13:28:00 +0100

    Clean movieRouter

--- api/routes/movieRouter.js
+++ api/routes/movieRouter.js
@@ -6,12 +6,9 @@ const MovieManager = require('../models/movieManager');
 
 movieRouter
   .post('/getMovie' , function(req, res) {
-    console.log(req.body.id);
-  //  getMoreData(req.body.id).then(done => {
       MovieManager.getMovie(req.body.id).then(result => {
         res.status(200).send(result);
       }).catch(error => res.status(404).send('errorInTheDb'))
-  //  })
   })
   .post('/list', function(req,res) {
     let filter = req.body.filter;
@@ -21,7 +18,6 @@ movieRouter
     if (req.body.filter.reverse) {
       reverse = 1;
     }
-    console.log('Limite : '+limit);
     // Manque le sort et le reverse -1 ou 1
     /*limit = 5;
     start = 0;
@@ -30,27 +26,5 @@ movieRouter
       res.status(200).send(result)
     })
   })
-// Check si on peut recuperer des infos en + avant de l'envoyer en front
-// Obliger de le mettre ici car nous sommes limite a 1 000 requetes / jour
 
-function getMoreData(id) {
-  return new Promise ((resolve, reject) => {
-    axios.get('http://www.omdbapi.com/?i='+id+'&apikey=c0120222')
-    .then(response => {
-      var data = response.data
-      let movie = {
-    //    public: data.Rated,
-  //      runtime: data.Runtime,
-  //      genre: data.Genre,
-  //      director: data.Director,
-  //      writer: data.Writer,
-  //      actors: data.Actors,
-        awards: data.Awards,
-    //    cover: data.Poster,
-  //      imdbRating: data.imdbRating
-      }
-      MovieManager.update(id, movie).then(done => resolve()).catch(error => reject(error));
-    })
-  })
-}
 module.exports = movieRouter;
