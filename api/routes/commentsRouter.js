0dd528a68d093e50e25f5c83746b4e55f9815cad
Wenceslas BARIDON <wbaridon@e3r2p18.42.fr>
2019-01-31 09:23:29 +0100

    Ask a token for commentsRouter

--- api/routes/commentsRouter.js
+++ api/routes/commentsRouter.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const commentsRouter = express.Router();
 const axios = require('axios');
+const tokenManager = require('../utils/token');
 
 const MovieManager = require('../models/movieManager');
 
 commentsRouter
   .post('/add' , function(req, res) {
-    console.log("bananier");
-    return ('ok');
+    tokenManager.decode(req.headers.authorization).then(token => {
+      console.log("bananier");
+      // Besoin de l'imdbId, username et comment ... En back on setup le timestamp
+      res.status(200).send('A faire')
+    }).catch(err => res.status(400).json({ error: 'token.invalidToken' }))
   })
   .post('/delete' , function(req, res) {
-    console.log("wakanda");
-    return ("oui");
+    tokenManager.decode(req.headers.authorization).then(token => {
+      console.log("wakanda");
+      // Besoin de l'imdbId, username et comment ... En back on setup le timestamp
+      res.status(200).send('A faire')
+    }).catch(err => res.status(400).json({ error: 'token.invalidToken' }))
   })
 
 module.exports = commentsRouter;
