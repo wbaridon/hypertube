95bf5bb6bd1c276b6206f45969cd6be0307e2ad5
Wenceslas BARIDON <wbaridon@e2r1p9.42.fr>
2019-02-06 12:55:12 +0100

    Movie id in db for user put string instead of number

--- api/models/user.js
+++ api/models/user.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MovieHistorySchema = new Schema({
-  id: Number
+  id: String
 });
 
 const WatchListSchema = new Schema({
-  id: Number
+  id: String
 });
 
 const UserSchema = new Schema({
