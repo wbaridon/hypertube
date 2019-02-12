2b32c056183916f9a208a0b7a9aaa6c1fc21d39f
Fabien RAHAINGOMANANA <frahaing@e2r4p21.42.fr>
2019-02-12 03:24:22 +0100

    added linter to backend + try to store converted file + fix streaming

--- api/routes/torrentRouter.js
+++ api/routes/torrentRouter.js
@@ -1,10 +1,12 @@
-var fs = require('fs');
+const fs = require('fs');
 const express = require('express');
+
 const torrentRouter = express.Router();
 const torrentStream = require('torrent-stream');
 
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
+
 ffmpeg.setFfmpegPath(ffmpegPath);
 const mimeTypes = require('../utils/mimeTypes.js');
 
@@ -16,116 +18,106 @@ function getExtention(fileName) {
 
 function getMagnet(hash) {
   const magnet = 'magnet:?xt=urn:btih:';
+  console.log(magnet.concat(hash));
   return magnet.concat(hash);
 }
 
 function isVideo(mime) {
-  return mimeTypes[mime] !== undefined ? true : false;
+  return mimeTypes[mime] !== undefined;
 }
 
 function findVideoFile(engine) {
   let file;
   engine.files.forEach((current) => {
-    let mime = getExtention(current.name);
+    const mime = getExtention(current.name);
     if (!isVideo(mime)) {
       return;
-    } else if (file && current.length < file.length) {
-      return;
-    } else {
-      file = current;
     }
+    if (file && current.length < file.length) {
+      return;
+    }
+    file = current;
   });
   return file;
 }
 
-function containsVideo(file, engine, res) {
-  if (!file) {
-    engine.removeAllListeners();
-    engine.destroy();
-    return res.send({ err: 'No video found on this torrent' });
-  }
-}
-
-function alreadyDownloaded(engine, res, file, id) {
-  if (fs.existsSync(`assets/torrents/${file.name}`)) {
+function alreadyDownloaded(res, hash, id) {
+  if (fs.existsSync(`assets/torrents/${hash}.mp4`)) {
     console.log('Movie already exists');
-    const data = { lastSeen: Date.now() }
-    const stream = fs.createReadStream(`assets/torrents/${file.name}`);
+    const data = { lastSeen: Date.now() };
+    const stream = fs.createReadStream(`assets/torrents/${hash}.mp4`);
     stream.pipe(res);
-    MovieManager.update(id, data)
+    MovieManager.update(id, data);
     return true;
-  } else {
-    const data = { lastSeen: Date.now(),
-      movieOnServer: true,
-      file: 'assets/torrents/' + file.name }
-    MovieManager.update(id, data)
-    return false;
   }
+
+  const data = {
+    lastSeen: Date.now(),
+    movieOnServer: true,
+    file: `assets/torrents/${hash}.mp4`,
+  };
+  MovieManager.update(id, data);
+  return false;
 }
 
 torrentRouter
   .get('/', async (req, res) => {
+    const {
+      videoHash,
+      id,
+    } = req.query;
+    const hash = videoHash;
+    const torrentMagnet = getMagnet(hash);
+    const engine = torrentStream(torrentMagnet);
 
-  const hash = req.query.videoHash;
-  const id = req.query.id;
+    engine.on('ready', () => {
+      const file = findVideoFile(engine);
+      if (!file) {
+        engine.removeAllListeners();
+        engine.destroy();
+        return res.send({ err: 'No video found on this torrent' });
+      }
 
-  const torrent_magnet = getMagnet(hash);
-  var engine = torrentStream(torrent_magnet);
+      file.select();
 
-  engine.on('ready', function() {
-    const file = findVideoFile(engine);
-    containsVideo(file, engine, res);
+      if (alreadyDownloaded(res, hash, id) === true) {
+        return null;
+      }
 
-    file.select();
+      const stream = file.createReadStream();
+      const converter = ffmpeg()
+        .input(stream)
+        .outputOptions('-movflags frag_keyframe+empty_moov')
+        .outputFormat('mp4')
+        .output(res)
+        .on('codecData', (codecData) => {
+          console.log('fluent-ffmpeg: CodecData:', codecData);
+        })
+        .on('start', (cmd) => { console.log('fluent-ffmpeg: Started:', cmd); })
+        .on('progress', (progress) => { console.log('fluent-ffmpeg: Progress:', progress.timemark, 'converted'); })
+        .on('end', () => {
+          console.log('Finished processing');
+        })
+        .on('error', (err, stdout, stderr) => {
+          console.log('ffmpeg, file:', file.path, ' Error:', '\nErr:', err, '\nStdOut:', stdout, '\nStdErr:', stderr);
+        });
 
-    if (alreadyDownloaded(engine, res, file, id) === true) {
-      return;
-    }
+      converter.inputFormat(getExtention(file.name).substr(1))
+        .audioCodec('aac')
+        .videoCodec('libx264')
+        .run();
 
-    const writeStream = fs.createWriteStream(`assets/torrents/${file.name}`);
-    const stream = file.createReadStream()
-    const converter = ffmpeg()
-      .input(stream)
-      .outputOptions('-movflags frag_keyframe+empty_moov')
-      .outputFormat('mp4')
-      .output(res)
-      .on('codecData', (codecData) => {
-         console.log('fluent-ffmpeg: CodecData:', codecData);
-      })
-      .on('start', (cmd) => { console.log('fluent-ffmpeg: Started:', cmd); })
-      .on('progress', (progress) => { console.log('fluent-ffmpeg: Progress:', progress.timemark, 'converted'); })
-      .on('end', function() {
-        console.log('Finished processing');
-      })
-      .on('error', (err, stdout, stderr) => {
-         console.log('ffmpeg, file:', file.path, ' Error:', '\nErr:', err, '\nStdOut:', stdout, '\nStdErr:', stderr);
+      const writeStream = fs.createWriteStream(`assets/torrents/${hash}.mp4`);
+      res.pipe(converter);
+      // stream.pipe(writeStream);
+
+      res.on('close', () => {
+        console.log('page closes, all processes killed');
+        converter.kill();
+        stream.destroy();
+        writeStream.destroy();
       });
-
-    console.log(getExtention(file.name).substr(1));
-
-    converter.inputFormat(getExtention(file.name).substr(1))
-    .audioCodec('aac')
-    .videoCodec('libx264')
-    .run();
-
-    res.pipe(converter);
-    stream.pipe(writeStream);
-
-    res.on('close', () => {
-      console.log('page closes, all processes killed');
-      converter.kill();
-      stream.destroy();
-      writeStream.destroy();
-    })
-    engine.on('idle', () => {
-      console.log(engine.swarm.downloaded);
-      const pathFolder = `assets/torrents/${hash}`;
-      fs.appendFile(pathFolder, file.data, function (err) {
-        if (err) throw err;
-        console.log("Movie file saved");
-      });
+      return null;
     });
   });
-})
-
 module.exports = torrentRouter;
